01a616ad1c478292ad020c32ec6a405f8404ecdf
Dorian <dorian@resalocal.com>
2022-12-17 00:30:42 +0100

    Setup test and projects

--- test/sparse-set.test.ts
+++ test/sparse-set.test.ts
@@ -6,4 +6,9 @@ describe('Sparse set', () => {
     it('can be created', () => {
         expect(() => SparseSet()).not.toThrowError()
     })
+    it('can insert', () => {
+        const sset = SparseSet()
+
+        expect(() => sset.insert(1)).not.toThrowError()
+    })
 });
