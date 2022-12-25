4995f3fad2a97de3f9a40c45f5f5083548134f0f
Dorian <dorian@resalocal.com>
2022-12-25 19:06:30 +0100

    Update bench

--- bench/ecs.ts
+++ bench/ecs.ts
@@ -5,7 +5,6 @@ import {addComponent, Component, removeComponent} from "../src/component";
 import {Types} from '../src/types'
 import {Query, registerQuery} from "../src/query";
 
-//  Destroy 100_000; entities
 {
     let world = World()
     let Position = Component({
@@ -16,20 +15,31 @@ import {Query, registerQuery} from "../src/query";
         x: Types.f32,
         y: Types.f32
     }, world)
-
+    let count = 0
     const MovementQuery = Query().all(Position, Velocity)
     registerQuery(MovementQuery, world)
 
-    run('World : Destroy 100_000; entities', () => {
-        for (let i = 0; i <= 100_000; i++) {
-            const eid = createEntity(world)
-            addComponent(Position, eid, world)
-            Position.x[eid] = 100
-            Position.y[eid] = 100
-            addComponent(Position, eid, world)
-            Position.x[eid] = 100
-            Position.y[eid] = 100
-            removeEntity(eid, world)
+    //https://github.com/ddmills/js-ecs-benchmarks/blob/master/suites/suite-add-remove.js
+    run('World : Velocity 2000 Iterations', () => {
+        for (let i = 0; i <= 2000; i++) {
+            const eid1 = createEntity(world)
+
+            addComponent(Position, eid1, world)
+            Position.x[eid1] = 100
+            Position.y[eid1] = 100
+            addComponent(Velocity, eid1, world)
+            Velocity.x[eid1] = 1.2
+            Velocity.y[eid1] = 1.7
+
+            //update mvmt system
+            for (let i = 0; i < MovementQuery.archetypes.length; i++) {
+                const arch = MovementQuery.archetypes[i]
+                for (let j = 0, l = arch.entities.dense.length; j < l; j++) {
+                    const id = arch.entities.dense[j];
+                    Position.x[id] += Velocity.x[id];
+                    Position.y[id] += Velocity.y[id];
+                }
+            }
         }
     })
 }
@@ -48,7 +58,7 @@ import {Query, registerQuery} from "../src/query";
     const MovementQuery = Query().all(Position, Velocity)
     registerQuery(MovementQuery, world)
 
-//https://github.com/ddmills/js-ecs-benchmarks/blob/master/suites/suite-add-remove.js
+    //https://github.com/ddmills/js-ecs-benchmarks/blob/master/suites/suite-add-remove.js
     run('World : Add/Remove 5000 Iterations', () => {
         for (let i = 0; i <= 5000; i++) {
             const eid1 = createEntity(world)
@@ -95,7 +105,7 @@ import {Query, registerQuery} from "../src/query";
     })
 }
 
-
+//  Destroy 100_000; entities
 {
     let world = World()
     let Position = Component({
@@ -106,31 +116,21 @@ import {Query, registerQuery} from "../src/query";
         x: Types.f32,
         y: Types.f32
     }, world)
-    let count = 0
+
     const MovementQuery = Query().all(Position, Velocity)
     registerQuery(MovementQuery, world)
 
-    //https://github.com/ddmills/js-ecs-benchmarks/blob/master/suites/suite-add-remove.js
-    run('World : Velocity 2000 Iterations', () => {
-        for (let i = 0; i <= 2000; i++) {
-            const eid1 = createEntity(world)
-
-            addComponent(Position, eid1, world)
-            Position.x[eid1] = 100
-            Position.y[eid1] = 100
-            addComponent(Velocity, eid1, world)
-            Velocity.x[eid1] = 1.2
-            Velocity.y[eid1] = 1.7
-
-            //update mvmt system
-            for (let i = 0; i < MovementQuery.archetypes.length; i++) {
-                const arch = MovementQuery.archetypes[i]
-                for (let j = 0, l = arch.entities.dense.length; j < l; j++) {
-                    const id = arch.entities.dense[j];
-                    Position.x[id] += Velocity.x[id];
-                    Position.y[id] += Velocity.y[id];
-                }
-            }
+    run('World : Destroy 100_000; entities', () => {
+        for (let i = 0; i <= 100_000; i++) {
+            const eid = createEntity(world)
+            addComponent(Position, eid, world)
+            Position.x[eid] = 100
+            Position.y[eid] = 100
+            addComponent(Position, eid, world)
+            Position.x[eid] = 100
+            Position.y[eid] = 100
+            removeEntity(eid, world)
         }
     })
 }
+
