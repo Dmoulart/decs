18c352becb0dab4427bc54cda4b0110d2421e53b
Dorian <dorian@resalocal.com>
2023-03-04 13:52:43 +0100

    CLEAN

--- test/workers/mutate-sparse-set.js
+++ test/workers/mutate-sparse-set.js
@@ -1,10 +1,6 @@
 import {parentPort} from "worker_threads";
 import {reconstructAtomicSparseSet} from "../../dist/collections/atomics/atomic-sparse-set.js";
 
-// const {
-//   reconstructAtomicSparseSet,
-// } = require("../../dist/collections/atomics/atomic-sparse-set.js");
-
 parentPort.on("message", (ssetParts) => {
   const sset = reconstructAtomicSparseSet(ssetParts);
   sset.remove(10);
