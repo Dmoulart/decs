f59cec3ea5fb326a0bf7398b804484183edcebf4
Dorian <dorian@resalocal.com>
2023-03-04 19:06:21 +0100

    create bitset reconstruction

--- src/collections/atomics/atomic-bit-set.ts
+++ src/collections/atomics/atomic-bit-set.ts
@@ -1,15 +1,39 @@
+import {isUint32Array} from "util/types";
 import {Bitset} from "../bit-set";
 
-export type AtomicBitset = Bitset;
+export type AtomicBitSet = Bitset;
+
+type AtomicBitSetBricks = AtomicBitSet["mask"];
+
+const isBitSetReconstruction = (
+  options: number | Uint32Array
+): options is Uint32Array => {
+  return isUint32Array(options);
+};
+
 /**
  * Create a new thread safe bitset.
  * It allows to make bitwise operations without the size limitations of a 32 integer.
  * @param size
  * @returns bitset
  */
-export const AtomicBitSet = (size = 4): AtomicBitset => {
-  let buffer = new SharedArrayBuffer(Uint32Array.BYTES_PER_ELEMENT * size);
-  let mask = new Uint32Array(buffer);
+export function AtomicBitSet(size?: number): AtomicBitSet;
+export function AtomicBitSet(mask: Uint32Array): AtomicBitSet;
+export function AtomicBitSet(
+  sizeOrMask: number | Uint32Array = 4
+): AtomicBitSet {
+  let buffer: SharedArrayBuffer;
+  let mask: Uint32Array;
+  let size: number;
+
+  if (isBitSetReconstruction(sizeOrMask)) {
+    mask = sizeOrMask;
+    size = mask.length;
+  } else {
+    size = sizeOrMask;
+    buffer = new SharedArrayBuffer(Uint32Array.BYTES_PER_ELEMENT * size);
+    mask = new Uint32Array(buffer);
+  }
 
   const resize = () => {
     buffer = new SharedArrayBuffer(Uint32Array.BYTES_PER_ELEMENT * (size + 2)); // ?? grow factor
@@ -67,9 +91,16 @@ export const AtomicBitSet = (size = 4): AtomicBitset => {
       return false;
     },
     clone() {
-      const clone = AtomicBitSet(size);
+      const clone = AtomicBitSet(size as number);
       clone.mask.set(mask);
       return clone;
     },
   };
-};
+}
+
+export const deconstructAtomicBitSet = ({
+  mask,
+}: AtomicBitSet): AtomicBitSetBricks => mask;
+
+export const reconstructAtomicBitSet = (mask: AtomicBitSetBricks) =>
+  AtomicBitSet(mask);
